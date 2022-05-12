9959de9e6f51b313c5901cd22ba580c83733dabe
Srihari <sriharib128@gmail.com>
2022-05-12 15:11:02 +0530

    q1

--- Assignment3/q1/q1_oj.c
+++ Assignment3/q1/q1_oj.c
@@ -1,59 +1,252 @@
-#include "deque.h"
-void printLevelOrder(tree Root)
+#ifndef BIN_TREE
+#define BIN_TREE
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct  BinTree* tree;
+
+struct BinTree{
+    tree left;
+    int data;
+    tree right;
+    int isBST;
+    long long int sum;
+    long long int max_value;
+    long long int min_value;
+};
+void PreOrder(tree T);
+void InOrder(tree T);
+void PostOrder(tree T);
+int depth(tree T);
+tree createBinary(int data);
+void delete(tree T);
+#endif
+
+#ifndef _DEQUE_H
+#define _DEQUE_H
+
+// #include "bintree.h"
+
+typedef struct dll* node;
+
+struct dll{
+    node prev;
+    node next;
+    tree data;
+};
+
+typedef struct stDQueue * Dq;
+
+struct stDQueue{
+    long long int size;
+    node head;
+    node tail;
+};
+
+Dq createStruct();
+void PushBack(Dq Q , tree T);
+tree PopFront(Dq Q);
+tree Front(Dq Q);
+int isEmpty(Dq Q);
+long long int Size(Dq Q);
+
+// long long int PopBack(Dq Q);
+// void PushFront(Dq Q , tree T);
+// long long int Back(Dq Q);
+void print(Dq P);
+// void print_reverse(Dq P);
+#endif
+// #include "deque.h"
+
+Dq createStruct()
 {
-    if (Root == NULL)
+    Dq Q =(Dq)malloc(sizeof(struct stDQueue));
+    Q->head=NULL;
+    Q->tail=NULL;
+    Q->size =0;
+    return Q;
+}
+
+void PushFront(Dq Q, tree T)
+{
+    node temp = (node)malloc(sizeof(struct dll));
+    temp->data = T;
+    Q->size = Q->size+1;
+    if(Q->size==1)
     {
+        Q->head=temp;
+        temp->next=NULL;
+        temp->prev=NULL;
+        Q->tail = temp;
         return;
     }
-
-    Dq Q = createStruct();
-    PushBack(Q, Root);
-
-    while (isEmpty(Q) != 1)
-    {
-        printf("data =%d sum = %lld max =%lld min = %lld\n", Q->head->data->data,Q->head->data->sum,Q->head->data->max_value,Q->head->data->min_value);
-
-        if (Q->head->data->left != NULL)
-        {
-            PushBack(Q, Q->head->data->left);
-        }
-        if (Q->head->data->right != NULL)
-        {
-            PushBack(Q, Q->head->data->right);
-        }
-        PopFront(Q);
-    }
-}
-
-int isLite(int left,int current , int right)
-{
-    if((left < current) && (current < right))
-        return 1;
-    if((left > current) && (current > right))
-        return 1;
-    return 0;
-}
-
-int Post_Order(tree T,long int * min)
-{
-    if(T== NULL)
-        return 0;
     else
     {
-        int left = Post_Order(T->left,min);
-        int right = Post_Order(T->right,min);
-        int current = T->data;
-        if(left ==0 && right == 0)
-            return current;
-        if(isLite(left,current,right))
-        {
-            if((*min)>(left + right + current))
-                *min = left + right + current;
-        }
-        return current;
+        temp->next = Q->head;
+        Q->head->prev = temp;
+        temp->prev = NULL;
+        Q->head = temp;
+        return;
     }
-    return 0;
 }
+void PushBack(Dq Q, tree T)
+{
+    node temp = (node)malloc(sizeof(struct dll));
+    temp->data = T;
+    Q->size = Q->size+1;
+    if(Q->size==1)
+    {
+        Q->head=temp;
+        temp->next=NULL;
+        temp->prev=NULL;
+        Q->tail = temp;
+        return;
+    }
+    else
+    {
+        temp->prev = Q->tail;
+        temp->next = NULL; 
+        Q->tail->next = temp;
+        Q->tail = temp;
+        return;
+    }
+}
+
+tree PopFront(Dq Q)
+{   
+    Q->size = Q->size -1;
+    tree element ;
+    if(Q->size ==0)
+    {
+        element = Q->head->data;
+        free(Q->head);
+        Q->head = NULL;
+        Q->tail = NULL;
+    }
+    else
+    {
+        node temp = Q->head;
+        Q->head = Q->head->next;
+        Q->head->prev = NULL;
+        element = temp->data;
+        free(temp);
+    }
+    return element;
+}
+
+tree PopBack(Dq Q)
+{   
+    Q->size = Q->size -1;
+    tree element ;
+    if(Q->size ==0)
+    {
+        element = Q->head->data;
+        free(Q->head);
+        Q->head = NULL;
+        Q->tail = NULL;
+    }
+    else
+    {
+        node temp = Q->tail;
+        Q->tail = Q->tail->prev;
+        Q->tail->next = NULL;
+        element = temp->data;
+        free(temp);
+    }
+    return element;
+}
+tree Front(Dq Q)
+{
+    return Q->head->data;
+}
+tree Back(Dq Q)
+{
+    return Q->tail->data;
+}
+int isEmpty(Dq Q)
+{
+    if(Q->size == 0)
+        return 1;
+    else
+        return 0;
+}
+
+long long int Size(Dq Q)
+{
+    return Q->size;
+}
+
+// #include "bintree.h"
+
+tree createBinary(int data)
+{
+    tree T = (tree)malloc(sizeof(struct BinTree));
+    T->data = data;
+    T->left = NULL;
+    T->right = NULL;
+    T->sum =0;
+    T->max_value =0;
+    T->min_value =0;
+    T->isBST = -1;
+    return T;
+}
+int max(int a , int b)
+{
+    if(a>=b)
+        return a;
+    else
+        return b;
+}
+
+void delete(tree T)
+{
+    if(T->left==NULL && T->right==NULL)
+    {   
+        printf("%d",T->data);
+        free(T);
+    }
+    else if(T->left != NULL)
+    {
+        delete(T->left);
+    }
+    else if(T->right !=NULL)
+    {
+        delete(T->right);
+    }
+}
+
+int depth(tree T)
+{
+    if(T == NULL)
+        return 0;
+    return 1 + max(depth(T->left),depth(T->right));
+}
+
+
+// void printLevelOrder(tree Root)
+// {
+//     if (Root == NULL)
+//     {
+//         return;
+//     }
+
+//     Dq Q = createStruct();
+//     PushBack(Q, Root);
+
+//     while (isEmpty(Q) != 1)
+//     {
+//         printf("data =%d sum = %lld max =%lld min = %lld\n", Q->head->data->data,Q->head->data->sum,Q->head->data->max_value,Q->head->data->min_value);
+
+//         if (Q->head->data->left != NULL)
+//         {
+//             PushBack(Q, Q->head->data->left);
+//         }
+//         if (Q->head->data->right != NULL)
+//         {
+//             PushBack(Q, Q->head->data->right);
+//         }
+//         PopFront(Q);
+//     }
+// }
 
 void check_asceding(tree current)
 {   
@@ -187,16 +380,14 @@ void check_descending(tree current)
     }
 }
 
-long long int checkLevelOrder(tree Root)
+long long int checkLevelOrder(tree Head)
 {   
     long long int min = INT_MAX;
-    if (Root == NULL)
-    {
+    if (Head == NULL)
         return 0;
-    }
 
     Dq Q = createStruct();
-    PushBack(Q, Root);
+    PushBack(Q, Head);
 
     while (isEmpty(Q) != 1)
     {
