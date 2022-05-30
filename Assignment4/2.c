eea768fc12d8b581db708aa53190616f4dfe75ec
Srihari <sriharib128@gmail.com>
2022-05-30 23:58:55 +0530

    2Q completed

--- Assignment4/2.c
+++ Assignment4/2.c
@@ -8,26 +8,50 @@ void swap(int *a,int*b)
     *b=temp;
 }
 
+int is_sorted(int A[],int N)
+{
+    int flag =0;
+    for(int i = 0 ; i<N-1;i++)
+    {
+        if(A[i]>A[i+1])
+        {
+            flag =1;
+            break;  
+        }
+    }
+    if(flag ==1)
+        return 1;
+    return 0;
+}
+
 int main()
 {
     int N,K;
-    scanf("%d %d",&N,&K);
+    scanf("%d",&N);
+    scanf("%d",&K);
     int A[N];
 
     for (int i = 0; i < N; i++)
         scanf("%d",&A[i]);
+        
     int count =0;
     for(int i=0;i<N;i++)
     {
+        if(is_sorted(A,N)==0)
+        {
+            printf("%d",count);
+            return 0;
+        }
         if(A[i]>K)
         {
             swap(&A[i],&K);
             count++;
         }
     }
-    for(int i=0;i<N-1;i++)
+    
+    for(int k=0;k<N-1;k++)
     {
-        if(A[i]>A[i+1])
+        if(A[k]>A[k+1])
         {
             printf("-1");
             return 0;
