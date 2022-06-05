224ce9367b50d2f8ea2d111db083ef333178b999
Srihari <sriharib128@gmail.com>
2022-06-05 09:07:32 +0530

    heap working

--- Assignment4/Heap/heap.c
+++ Assignment4/Heap/heap.c
@@ -55,43 +55,33 @@ void Insert(heap H,ElementType a)
 
 void AdjustHeap(heap H, int pos)
 {
-    if( (2*pos +2) >(H->Size-1))// LAST NODE HAS ONE CHILD ONLY
+    if( (2*pos +2) >(H->Size-1))
     {
-        if( (2*pos +1) >(H->Size-1))
+        if( (2*pos +1) >(H->Size-1))//LAST NODE HAS NO CHILD
             return;
-        else
+        else // LAST NODE HAS ONE CHILD ONLY
         {
             if(H->Elements[pos]>H->Elements[2*pos +1])
                 swap( &H->Elements[pos], &H->Elements[2*pos +1]);
             return;
         }
     }
-    // else if((2*pos +2)== (H->Size-1))
-    // {
-    //     if(H->Elements[2*pos +1] > H->Elements[2*pos +2])
-    //     {
-    //         swap( &H->Elements[pos], &H->Elements[2*pos +2]);
-    //         return;
-    //     }
-    //     else
-    //     {
-    //         swap( &H->Elements[pos], &H->Elements[2*pos +1]);
-    //         return;
-    //     }
-    // }
     else
     {
-        if(H->Elements[2*pos +2] > H->Elements[2*pos +1])
+        if(H->Elements[pos]>minimum(H->Elements[2*pos +2],H->Elements[2*pos +1]))
         {
-            swap( &H->Elements[pos], &H->Elements[2*pos +1]);
-            AdjustHeap(H,(2*pos +1));
-            return;
-        }
-        else
-        {
-            swap( &H->Elements[pos], &H->Elements[2*pos +2]);
-            AdjustHeap(H,(2*pos +2));
-            return;
+            if(H->Elements[2*pos +2] > H->Elements[2*pos +1])
+            {
+                swap( &H->Elements[pos], &H->Elements[2*pos +1]);
+                AdjustHeap(H,(2*pos +1));
+                return;
+            }
+            else
+            {
+                swap( &H->Elements[pos], &H->Elements[2*pos +2]);
+                AdjustHeap(H,(2*pos +2));
+                return;
+            }
         }
     }
     return ;
@@ -109,30 +99,14 @@ ElementType DeleteMin(heap H)
         return min;
     }
     else if(H->Size >=2)
-    {
+    {   
+        // printf("[Size=%d , %d]",H->Size,H->Elements[H->Size-1]);
         min = H->Elements[0];
         H->Elements[0]=H->Elements[H->Size-1];//effectively creating a hole and pecolating it down
         H->Elements[H->Size-1]= INT_MAX;
+        // printHeap(H);
         AdjustHeap(H,0);
-        // if(H->Elements[1]>H->Elements[2])
-        // {
-        //     swap(&H->Elements[0],&H->Elements[2]);
-        //     if(H->Size!=2)
-        //         AdjustHeap(H,2);
-        //     if(H->Size==2)
-        //         H->Size--;
-        // }
-        // else
-        // {
-        //     min = H->Elements[0]; 
-        //     if(H->Size ==2)
-        //         H->Elements[1]=H->Elements[2];
-        //     else
-        //     {
-        //         H->Elements[1] = INT_MAX;
-        //         AdjustHeap(H,1);
-        //     }
-        // }
+        // printHeap(H);
         H->Size--;
         return min;
     }
@@ -142,36 +116,36 @@ ElementType DeleteMin(heap H)
 void printHeap(heap H)
 {
     printf(" ( %d %d ) ",H->Capacity,H->Size);
-    for(int i=0;i<=H->Size;i++ )
+    for(int i=0;i<H->Size;i++ )
     {
         printf("%d ",H->Elements[i]);
     }
     // printf("\n");
 }
 
-int main()
-{
-    int A[]={1,2,3,5,4,3,2,1,2};
-    int elements = sizeof(A)/sizeof(int);
-    heap H = Initialize(elements);
-    for(int i=0;i<elements;i++)
-    {
-        Insert(H,A[i]);
-        printHeap(H);
-        printf("\n");
-    }
-    printf("------------------------------------\n");
-    printf("------------------------------------\n");
-    while ((H->Size>0))
-    {
-        printHeap(H);
-        printf(" ==>%d==> ",DeleteMin(H));
-        printHeap(H);
-        printf("\n");
-    }
-    printHeap(H);
-        printf(" ==>%d==> ",DeleteMin(H));
-        printHeap(H);
-        printf("\n");
-    return 0;
-}
+// int main()
+// {
+//     int A[]={1,-1,2,4};
+//     int elements = sizeof(A)/sizeof(int);
+//     heap H = Initialize(elements);
+//     for(int i=0;i<elements;i++)
+//     {
+//         Insert(H,A[i]);
+//         printHeap(H);
+//         printf("\n");
+//     }
+//     printf("------------------------------------\n");
+//     printf("------------------------------------\n");
+//     while ((H->Size>0))
+//     {
+//         printHeap(H);
+//         printf(" ==>%d==> ",DeleteMin(H));
+//         printHeap(H);
+//         printf("\n");
+//     }
+//     printHeap(H);
+//         printf(" ==>%d==> ",DeleteMin(H));
+//         printHeap(H);
+//         printf("\n");
+//     return 0;
+// }
