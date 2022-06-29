9ae30df4b6b91873b235dd7e8f19da1a9eb0335b
Srihari <sriharib128@gmail.com>
2022-06-29 13:42:22 +0530

    .

--- Sorting/quickSort_Middle.c
+++ Sorting/quickSort_Middle.c
@@ -14,29 +14,53 @@ int sort(int A[],int start , int end , int pivot)
     int j = end;
 
     int temp= A[pivot];
+    int *temp_ptr = &A[pivot];
     while(i<j)
     {
+        while(A[i]<=temp)
+        {
+            i++;
+            if(i>=end)
+                break;
+        }
+        while (A[j]>=temp)
+        {
+            j--;
+            if(j<=start)
+                break;
+        }
+        if(i>=end)
+                break;
+        if(j<=start)
+                break;
         if(A[i]>temp && A[j]<temp)
         {
             swap(&A[i],&A[j]);
-            i++;
-            j--;
+            if(i==pivot)
+            {
+                temp_ptr = &A[j];
+            }
+            if(j==pivot)
+            {
+                temp_ptr = &A[i];
+            }
         }
-        else
-            i++;
     }
+    swap(temp_ptr,&A[j]);
+    return j;
 }
 
 void quicksort(int A[],int start , int end)
 {
     if(start<end)
     {
-        // int pivot = (start+end)/2;
-        int pivot = start;
+        int pivot = (start+end)/2;
+        // int pivot = start;
         // int pivot = end;
-        sort(A,start,end,pivot);
-        quicksort(A,start,pivot);
-        quicksort(A,pivot+1,end);
+        int pivotindex;
+        pivotindex =sort(A,start,end,pivot);
+        quicksort(A,start,pivotindex-1);
+        quicksort(A,pivotindex+1,end);
     }
 }
 
