20499bc31adfff04f285cd68a91153b9498acdd8
Srihari <sriharib128@gmail.com>
2022-07-08 14:58:34 +0530

    Q2

--- Assignment5/2.cpp
+++ Assignment5/2.cpp
@@ -3,5 +3,21 @@ using namespace std;
 
 int main()
 {
+    int t;
+    scanf("%d",&t);
+    while (t--)
+    {
+        int N,K;
+        scanf("%d %d",&N,&K);
+        vector <int> vertex[N+1];
+        for(int i=0;i<N-1;i++)
+        {
+            int u,v;
+            scanf("%d %d",&u,&v);
+            vertex[u].push_back(v);
+            vertex[v].push_back(u);
+        }
+        
+    }
     
 }
--- Assignment5/5.cpp
+++ Assignment5/5.cpp
@@ -7,10 +7,24 @@ typedef pair<long long int,long long int> pi;
 
 typedef vector<pair<long long int , int>> ver;//(weight , node)
 
+long long int mod(long long int x)
+{
+    if(x<0)
+        x=x*(-1);
+    return x;
+}
+
 long long int weight(pi p1,pi p2)
 {
-    return p1.first;
+    long long int a = mod(p1.first - p2.first);
+    long long int b = mod(p1.second - p2.second);
+
+    if(a>b)
+        return b;
+    return a;
+
 }
+
 
 int main()
 {   
@@ -26,6 +40,7 @@ int main()
         {
             long long int x,y;
             scanf("%lld %lld",&x,&y);
+            // printf("%d  -  (%lld,%lld)\n",i,x,y);
             vx.push_back({x,{i,{x,y}}});
             vy.push_back({y,{i,{x,y}}});
         }
@@ -47,7 +62,6 @@ int main()
 
             vertex[i_1].push_back({temp,i_2});
             vertex[i_2].push_back({temp,i_1});
-            // vertex[vx[i].second.first].push_back({weight(vx[i].second.second.first,vx[i].second.second.second),vx[i].second.first});
         }
 
         for(int i=0;i<N-1;i++)
@@ -61,13 +75,71 @@ int main()
             int i_2 = vy[i+1].second.first;
 
             long long int temp = weight({x_1,y_1},{x_2,y_2});
-
-            vertex[i_1].push_back({temp,i_2});
-            vertex[i_2].push_back({temp,i_1});
-            // vertex[vx[i].second.first].push_back({weight(vx[i].second.second.first,vx[i].second.second.second),vx[i].second.first});
+            int flag=0;
+            for(int j=0;j<vertex[i_1].size();j++)
+            {
+                if(vertex[i_1][j].second==i_2)
+                    flag =1;
+            }
+            if(flag==0)
+            {
+                vertex[i_1].push_back({temp,i_2});
+                vertex[i_2].push_back({temp,i_1});
+            }
         }
-
         
+        // for(int i=0;i<N;i++)
+        // {
+        //     printf("%d",i);
+        //     for(int j=0;j<vertex[i].size();j++)
+        //         printf("\t>(%lld,%d)",vertex[i][j].first,vertex[i][j].second);
+        //     printf("\n");
+
+        // }
+
+        //PRIMS ALGORITHMS
+        int visited[100005] ={};
+
+        // priority_queue <pair<long long int ,int>> pq;//(weight ,node)
+        priority_queue<pair<long long int ,int>, vector<pair<long long int ,int>>, greater<pair<long long int ,int>> > pq;
+
+        long long int ans =0;
+        pq.push({0,0});
+        int vis=0;
+        while(pq.size()!=0)
+        {   
+            long long int cur_weight = pq.top().first;
+            int cur_node = pq.top().second;
+            pq.pop();
+            // printf("cur = (%lld,%d)\n",cur_weight,cur_node);
+            
+            
+            if(visited[cur_node]==1)
+            {   
+                // printf("\t>already visited\n");
+                continue;
+            }
+
+
+            ans= ans+cur_weight;
+            // printf("\t> added it to sum=%lld\n",ans);
+            visited[cur_node]=1;
+            vis++;
+            if(vis>=N)
+                break;
+
+
+            for(int i=0;i<vertex[cur_node].size();i++)
+            {
+                if(visited[vertex[cur_node][i].second]!=1)
+                {
+                    pq.push({vertex[cur_node][i].first,vertex[cur_node][i].second});
+                    // printf("\t\t>added (%lld,%d)\n",vertex[cur_node][i].first,vertex[cur_node][i].second);
+                }
+            }
+        }
+        printf("%lld\n",ans);
 
     }
+    return 0;
 }
